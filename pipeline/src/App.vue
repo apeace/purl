b453b84ce9916f1c4ab44ee97b9f2c6d26d8490b
Andrew Peace <andrewpeaceweb@gmail.com>
2026-02-21 17:40:45 -0500

    First commit

---
+++ pipeline/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Customer App</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { hello } from '@pipeline/lib';
+
+const message = hello();
+</script>
