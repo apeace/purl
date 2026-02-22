f1abdf77a320a0f6b62da5081b522cb827c11bfb
Andrew Peace <andrewpeaceweb@gmail.com>
2026-02-21 20:58:10 -0500

    Live reloading.

--- pipeline/vite.config.js
+++ pipeline/vite.config.js
@@ -3,4 +3,12 @@ import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: true,
+    // Polling is needed for live-reload inside Docker containers,
+    // where native FS events from mounted volumes may not fire.
+    watch: {
+      usePolling: true,
+    },
+  },
 });
