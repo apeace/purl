1a61c6691ccc405228b9c2e226d861df11ab1d49
Andrew Peace <andrewpeaceweb@gmail.com>
2026-02-25 19:39:02 -0500

    Fix reset-orgs on prod

--- deploy/cmd.sh
+++ deploy/cmd.sh
@@ -16,8 +16,15 @@ fi
 CMD="$1"
 shift
 
+EXTRA_ARGS=()
+CLIENTS_FILE="$SCRIPT_DIR/../api/clients.json"
+if [ -f "$CLIENTS_FILE" ]; then
+  EXTRA_ARGS+=("-e" "PURL_CLIENTS_JSON=$(tr -d '\n' < "$CLIENTS_FILE")")
+fi
+
 docker compose \
   -f "$SCRIPT_DIR/docker-compose.prod.yml" \
   run --rm \
+  "${EXTRA_ARGS[@]}" \
   api \
   ./bin/"$CMD" "$@"
