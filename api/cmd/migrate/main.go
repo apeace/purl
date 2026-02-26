c994781cbde0a9bb2ee3b81fde6d13bd0225d876
Andrew Peace <andrewpeaceweb@gmail.com>
2026-02-25 22:31:52 -0500

    Don't run migrations on dev, add migration cmd

---
+++ api/cmd/migrate/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"github.com/pressly/goose/v3"
+)
+
+func main() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is required")
+	}
+
+	db, err := sql.Open("pgx", dsn)
+	if err != nil {
+		log.Fatalf("open db: %v", err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatalf("ping db: %v", err)
+	}
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatalf("goose set dialect: %v", err)
+	}
+	if err := goose.Up(db, "migrations"); err != nil {
+		log.Fatalf("goose up: %v", err)
+	}
+	log.Println("migrations applied")
+}
